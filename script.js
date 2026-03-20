67ffb92673885e90d5a93f3a4d41f126ab1a81a8
Alisa Leung <alisal.singyee@gmail.com>
2026-03-20 08:18:24 -0700

    ready for deployment, will add faq later

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const navMenu = document.getElementById("navMenu");
     const navBar = document.getElementById("navBar");
     const navLinks = document.querySelectorAll(".navLink");
+    const questions = document.getElementsByClassName("question");
 
     navMenu.addEventListener("click", () => {
         navBar.classList.toggle("active");
@@ -12,4 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
             navBar.classList.remove("active");
         });
     });
+
+    /*
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            let answer = this.nextElementSibling;
+            let faqItem = this.parentElement;
+            if (answer.style.maxHeight) {
+                answer.style.maxHeight = null;
+            } else {
+                answer.style.maxHeight = answer.scrollHeight + "px";
+            }
+        });
+    }
+    */
 });
